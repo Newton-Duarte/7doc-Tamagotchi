63ca82dbba82e4b75ff2c1f3cce21845f1c780f3
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-08-09 10:28:16 -0300

    feat: add tamagotchi view method to program

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -4,39 +4,13 @@ using System.Collections.Generic;
 using System.Text.Json;
 using Tamagotchi;
 using Tamagotchi.Model;
+using Tamagotchi.View;
 
 class Program
 {
-    public static string name = "";
-    public static List<Pokemon> adoptedPokemons = new List<Pokemon>();
-
     static void Main(string[] args)
     {
-        Console.WriteLine("Qual o seu nome?");
-        name = Console.ReadLine();
-
-        Menu.Open();
-    }
-
-    public static void ShowAdoptedPokemons()
-    {
-        if (adoptedPokemons.Count == 0)
-        {
-            Console.WriteLine("Lista vazia");
-            return;
-        }
-
-        Console.WriteLine("----------- Pokemons Adotados ----------");
-
-        foreach (var pokemon in adoptedPokemons)
-        {
-            Console.WriteLine($"{Capitalize(pokemon.Name)}");
-        }
-    }
-
-    public static string Capitalize(string str)
-    {
-        if (str == null) return "-";
-        return $"{str[0].ToString().ToUpper()}{str.Substring(1)}";
+        TamagotchiView.Welcome();
+        TamagotchiView.Menu();
     }
 }
