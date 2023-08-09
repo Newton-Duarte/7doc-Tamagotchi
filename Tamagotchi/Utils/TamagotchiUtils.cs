21ff1919f13b5daa0471b1f8128303d0dfc8b09c
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-08-09 10:27:10 -0300

    feat: add tamagotchi utils

---
+++ Tamagotchi/Utils/TamagotchiUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi.Utils
+{
+    internal static class TamagotchiUtils
+    {
+        public static string Capitalize(string str)
+        {
+            return $"{str[0].ToString().ToUpper()}{str.Substring(1)}";
+        }
+    }
+}
